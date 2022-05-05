397ee7919cc4b9d6b9bdd8199a4038eefd2d5398
dianay-711 <dyusupov@ucsc.edu>
2022-05-04 20:24:36 -0700

    Completing lab9

---
+++ lab9/js/js-for-the-web.js
@@ -0,0 +1,23 @@
+/**
+ * Author:    Diana Yusupova
+ * Created:   05.04.2022
+ * License: Public Domain
+ **/
+
+ // assigning output div to outputEl
+var outputEl = document.getElementById("output");
+ // create new element names new1El
+var new1El = document.createElement("p");
+ // change html attribute to say something new
+new1El.innerHTML = "Heyo";
+ // create another new element named new2El
+new2El = document.createElement("h");
+ // change html attribute of new2El to say something else
+new2El.innerHTML = "What's up with it";
+ // append both new elements to output <div>
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+ // change the font size
+new1El.style.fontSize = "20pt";
+new2El.style.dontSize = "20pt";
